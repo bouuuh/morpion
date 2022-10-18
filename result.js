1c69effd59b3d90f75d1424df050c2ed68e41ab3
Candice <candice.doise@gmail.com>
2022-10-18 14:46:20 +0200

    seems donebut wasn't

--- result.js
+++ result.js
@@ -1,5 +1,6 @@
 let whoWon = document.querySelector(".whowon");
-
+let local = JSON.parse(localStorage.getItem("getParameters"));
+let announce = document.querySelector(".announce");
 //revenir au menu principal
 let back = document.querySelector(".back");
 
@@ -10,10 +11,12 @@ back.addEventListener("click", function() {
 
 
 if ((JSON.parse(localStorage.winPlayer1) > JSON.parse(localStorage.winPlayer2)) ) {  
-    whoWon.innerHTML = "Joueur 1 !"
+    whoWon.innerHTML = "Bravo Joueur 1 ! " 
+    announce.innerHTML = localStorage.winPlayer1 + " victoire sur " + local.numberGames;
 }
 else if ((JSON.parse(localStorage.winPlayer1) < JSON.parse(localStorage.winPlayer2)) ) {   
     whoWon.innerHTML = "Joueur 2 !"
+    announce.innerHTML = localStorage.winPlayer2 + " victoire sur " + local.numberGames;
 }    
 else if ((JSON.parse(localStorage.winPlayer1) === JSON.parse(localStorage.winPlayer2)) ) {
     whoWon.innerHTML = "Personne !"
